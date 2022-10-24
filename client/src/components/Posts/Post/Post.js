5686c6b1ba79335727f1c680be2a17e32d3645e6
Thalyonn <thalyonns@gmail.com>
2022-10-25 01:38:49 +0800

    Display Posts Update
    Added Post Display Functionality

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,9 +1,42 @@
 import React from 'react';
 
+<<<<<<< Updated upstream
 const Post = () => {
     return(
         <h1>Post</h1>
+=======
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import { mergeClasses } from '@material-ui/styles';
+import useStyles from './styles';
+const Post = ({ post }) => {
+    const classes = useStyles();
+    return(
+        <Card className={classes.card}>
+            <CardMedia className={classes.media} image={postMessage.selectedFile} title={Post.title} />
+            <div className={classes.overlay}>
+                <Typography variant='h6'>{post.title}</Typography>
+                <Typography variant='body1'>{post.creator}</Typography>
+                <Typography variant='body2'>{post.date}</Typography>
+
+            </div>
+            <div className={classes.overlay2}>
+                <Button style={{color: 'white'}} size="small" onClick={() => {}}>
+                    <MoreHorizIcon fontSize="default" />
+                </Button>
+            </div>
+            <div className={classes.details}>
+                <Typography variant="body2" color="textSecondary">{post.tags}</Typography>
+                
+            </div>
+            <CardContent>
+            <Typography className={classes.title} variant="h5" gutterBottom>{post.description}</Typography>
+            <Typography className={classes.title} variant="body1" gutterBottom>{post.price}</Typography>
+            </CardContent>
+            
+            
+        </Card>
+>>>>>>> Stashed changes
     );
-}
+};
 
 export default Post;
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,11 +1,23 @@
 import React from 'react';
+<<<<<<< Updated upstream
+=======
+import { Grid, CircularProgress } from '@material-ui/core';
+
+>>>>>>> Stashed changes
 import Post from './Post/Post.js';
 import { useSelector } from 'react-redux';
 import useStyles from './styles';
 const Posts = () => {
+<<<<<<< Updated upstream
     const posts= useSelector((state)=> state.posts) //gets the posts from the posts.js reducer by extracting it from the store state declared in main index
 
     console.log(posts);
+=======
+    const posts= useSelector((state)=> state.posts); //gets the posts from the posts.js reducer by extracting it from the store state declared in main index
+    const classes = useStyles();
+    
+   
+>>>>>>> Stashed changes
     return(
         <>
         <h1>Posts</h1>
