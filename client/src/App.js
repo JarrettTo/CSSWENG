922f16a26b96f1240b559e51904fb34c23c82622
Thalyonn <thalyonns@gmail.com>
2022-11-05 21:54:08 +0800

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Container } from '@material-ui/core';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import Auth from './components/Auth/Auth';
@@ -14,11 +14,13 @@ const App = () => {
     
     return (
         <BrowserRouter>
-            <Container   maxWidth="lg"> {/*equivalent of a div, lg means large*/} 
+            <Container   maxWidth="xl"> {/*equivalent of a div, lg means large*/} 
                 <Navbar />
                 <SearchBar />
                 <Switch>
-                    <Route path='/' exact component={Home} />
+                    <Route path="/" exact component={() => <Redirect to ="/posts"/>} />
+                    <Route path="/posts" exact component={Home} />
+                    <Route path="/posts/search" exact component={Home} />
                     <Route path="/auth" exact component={Auth} />
                     <Route path="/posts/:id" component={PostDetails} />
                 </Switch>
