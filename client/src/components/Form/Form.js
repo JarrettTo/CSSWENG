f25a8cffe8d6a2e0743918f92ac7234152b20520
Thalyonn <thalyonns@gmail.com>
2022-12-01 22:33:41 +0800

    Form Validation Add Post

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -13,6 +13,7 @@ const Form = ({currentID, setCurrentID}) => {
     const history = useHistory();
     const user = JSON.parse(localStorage.getItem('profile'))
     console.log("Post:" + post);
+    const [error, setError] = React.useState(false);
     const [postData, setPostData] = useState({      //initializes postData to the ff values. we set "setPostData" as the setter function for the state variable "postData"
         title: '',
         date: '',
@@ -58,14 +59,23 @@ const Form = ({currentID, setCurrentID}) => {
     
     const handleSubmit = async (e) => {
         e.preventDefault();
-    
+        console.log("logging desc");
+        console.log(postData.description);
+        if(!postData.description || !postData.creator || !postData.date || !postData.endDate || !postData.maxAttendees || !postData.price || !postData.title)
+        {
+            console.log("no desc");
+            setError(true);
+            return false;
+        }
         if(currentID!=null){                    
             dispatch(updatePost(currentID, {...postData,name: user?.result?.id}));
             clear();
+            setError(false);
         }
         else{
             dispatch(createPost({...postData,name: user?.result?.id}));             //dispatches our createPost function from our actions with the parameter of our new object created by filling up the form
             clear();
+            setError(false);
         }
       };
        
@@ -83,14 +93,15 @@ const Form = ({currentID, setCurrentID}) => {
         <Paper className={classes.paper}>       {/*adds a white bg*/}
             <form autoComplete='off' noValidate className={classes.form} onSubmit={handleSubmit}>
                 <Typography variant='h6'>Add an event:</Typography>
-                <TextField name='title' variant='outlined' label="Title" fullWidth value={postData.title} onChange={(e)=>{setPostData({...postData ,title: e.target.value})}}/>
-                <TextField name='date' variant='outlined' label="Start Date YYYY-MM-DD HH:mm" fullWidth value={postData.date} onChange={(e)=>{setPostData({...postData ,date: e.target.value})}}/>
-                <TextField name='endDate' variant='outlined' label="End Date YYYY-MM-DD HH:mm" fullWidth value={postData.endDate} onChange={(e)=>{setPostData({...postData ,endDate: e.target.value})}}/>
-                <TextField name='creator' variant='outlined' label="Creator" fullWidth value={postData.creator} onChange={(e)=>{setPostData({...postData ,creator: e.target.value})}}/>   {/*e.target.value basically just takes the new value of the textfrield. the onchange attribute ensures that it only changes whenever there is a change to the textfield*/}
-                <TextField name='description' variant='outlined' label="Description" multiline rows ={4} maxRows={4} fullWidth value={postData.description} onChange={(e)=>{setPostData({...postData ,description: e.target.value})}}/>
-                <TextField name='price' variant='outlined' label="Price" fullWidth value={postData.price} onChange={(e)=>{setPostData({...postData ,price: e.target.value})}}/>
-                <TextField name='maxAttendees' variant='outlined' label="Max Attendees" fullWidth value={postData.maxAttendees} onChange={(e)=>{setPostData({...postData ,maxAttendees: e.target.value})}}/>
+                <TextField error={error && !postData.title} name='title' variant='outlined' label="Title" fullWidth value={postData.title} onChange={(e)=>{setPostData({...postData ,title: e.target.value})}}/>
+                <TextField error={error && !postData.date} name='date' variant='outlined' label="Start Date YYYY-MM-DD HH:mm" fullWidth value={postData.date} onChange={(e)=>{setPostData({...postData ,date: e.target.value})}}/>
+                <TextField error={error && !postData.endDate} name='endDate' variant='outlined' label="End Date YYYY-MM-DD HH:mm" fullWidth value={postData.endDate} onChange={(e)=>{setPostData({...postData ,endDate: e.target.value})}}/>
+                <TextField error={error && !postData.creator} name='creator' variant='outlined' label="Creator" fullWidth value={postData.creator} onChange={(e)=>{setPostData({...postData ,creator: e.target.value})}}/>   {/*e.target.value basically just takes the new value of the textfrield. the onchange attribute ensures that it only changes whenever there is a change to the textfield*/}
+                <TextField error={error && !postData.description} name='description' variant='outlined' label="Description" multiline rows ={4} maxRows={4} fullWidth value={postData.description} onChange={(e)=>{setPostData({...postData ,description: e.target.value})}}/>
+                <TextField error={error && !postData.price} name='price' variant='outlined' label="Price" fullWidth value={postData.price} onChange={(e)=>{setPostData({...postData ,price: e.target.value})}}/>
+                <TextField error={error && !postData.maxAttendees} name='maxAttendees' variant='outlined' label="Max Attendees" fullWidth value={postData.maxAttendees} onChange={(e)=>{setPostData({...postData ,maxAttendees: e.target.value})}}/>
                 <TextField name='tags' variant='outlined' label="Tags" fullWidth value={postData.tags} onChange={(e)=>{setPostData({...postData ,tags: e.target.value.split(',')})}}/>
+                {error && <span>One or more required field is empty</span> }
                 <div className={classes.fileInput}>
                     <FileBase type ="file"multiple={false} onDone={({base64})=> setPostData({...postData, selectedFile: base64})}/>
                 </div>
