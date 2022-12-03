98b7e9f9906ba60f36350d67a7b080522af33ade
chelseaalabanza <chelsea_alabanza@dlsu.edu.ph>
2022-12-03 17:53:39 +0800

    Few edits on showpage
    Showpage is not yet done

--- client/src/components/PostDetails/PostDetails.js
+++ client/src/components/PostDetails/PostDetails.js
@@ -2,13 +2,16 @@ import React, { useEffect } from "react";
 import FileBase from 'react-file-base64';
 import useStyles from './styles';
 import { useState } from "react";
-import { Paper, Typography, CircularProgress, Divider, TextField, Button} from '@material-ui/core';
+import { Paper, Typography, CircularProgress, Divider, TextField, Button, CssBaseline} from '@material-ui/core';
 import { useDispatch, useSelector } from 'react-redux';
 import { Container, Grow, Grid} from '@material-ui/core';
-import moment from 'moment';
+import moment from 'moment';    
 import { useParams, useHistory } from 'react-router-dom';
 import { getPost, getPosts, registerPost } from "../../actions/posts";
 import { getTxn } from "../../actions/transactions";
+import regBg from '../../images/regBg.png';
+
+
 const PostDetails = () => {
     const [user,setUser]=useState(JSON.parse(localStorage.getItem('profile')));
     
@@ -22,6 +25,7 @@ const PostDetails = () => {
     const [txn, setTxn]=useState(getTxn(id));
     const [trigger,setTrigger]=useState(false);
     const selPost=posts.find((e)=>{ return e._id==id});
+    // const mainPub = {selPost?.selectedFile}.replace('data:image/png;base64,', '');
     
     useEffect(()=>{
         console.log(posts)
@@ -43,35 +47,173 @@ const PostDetails = () => {
         
     }
     return (
+       
+        <Grid>
+            {/* {selPost?.title} */}
+            {/* {selPost?.description} */}
+            {/* {selPost?.price} */}
+            {/* {selPost?.date} */}
+            {/* {selPost?.tags} */}
+            {/* {selPost?.registeredUsers} */}
+            {/* {selPost?.selectedFile} */}
 
-        <Grid container alignItems="stretch" spacing={3}>
-            {selPost?.title}
-            {selPost?.description}
-            {selPost?.price}
-            {selPost?.date}
-            {selPost?.tags}
-            {selPost?.registeredUsers}
-            <Typography>Status: {txn.status}</Typography>
-            {(!selPost?.registeredUsers?.find((e)=> e==user.result._id) && !selPost?.acceptedUsers?.find((e)=> e==user.result._id))?(
-                <>
-                {(!user?.result?.dlsu || user?.result?.claimed) ? (
-                    <form autoComplete='off' noValidate className={classes.form} onSubmit={handleSubmit}>
-                        <Typography>Register for event:</Typography>
-                        <Typography>Payment Details: 09270164346 GCASH JUSTIN TO</Typography>
-                        <Typography>PROOF OF PAYMENT:</Typography>
-                        <div className={classes.fileInput}>
-                            <FileBase type ="file"multiple={false} onDone={({base64})=> setFile(base64)}/>
-                        </div>
-                        <Button className={classes.buttonSubmit} variant="container" color="primary" size="large" type="submit" fullWidth>Register</Button>
-                        
-                    </form>
-                ): <Button className={classes.buttonSubmit} variant="container" color="primary" size="large" type="submit" onClick={handleSubmit} fullWidth>Register</Button> }
-                
-                
-                </>
-            ):<Button className={classes.buttonSubmit} variant="container" color="primary" size="large" type="submit" onClick={handleSubmit} fullWidth>Unregister</Button> }
-            
+            <CssBaseline />
+
+            <main>
+                {/* SHOW TITLE */}
+                <div>
+                    <Container>
+                        <Typography gutterBottom className={classes.title} >
+                            {selPost?.title}
+                        </Typography>
+                    </Container>
+                </div>
+
+                {/* 1ST SECTION: SHOW INFORMATION INSIDE CLOUDY CONTAINER */}
+                <div>
+                    <Container className={classes.infoBox}>
+                        <Container className={classes.innerInfo}>
+                            {/* left */}
+                            <Container className={[classes.innerBox, classes.left]}> 
+
+                                {/* insert uploaded images by the creator */}
+                                <Container className={classes.descImg}>-insert image-</Container>
+                                
+                            </Container>
+
+                            {/* right */}
+                            <Container className={[classes.innerBox, classes.right]}>
+                                <Container className={classes.innerRight}>
+                                    <Container>
+                                        <Typography className={classes.eventBy}>EVENT BY {selPost?.creator}</Typography>
+                                    </Container>
+
+                                    <Container>
+                                        <Typography className={classes.innerTitle}>{selPost?.title}</Typography>
+                                    </Container>
+
+                                    <Container>
+                                        <Typography className={classes.venue}>HAPPENING AT -insert venue-</Typography>
+                                    </Container>
+                                    
+                                    <Container>
+                                        <Typography className={classes.description}>{selPost?.description}</Typography>
+                                    </Container>
+
+                                    <Container className={classes.bottomInfo}>
+                                        <Container className={classes.price}>
+                                            <Typography className={classes.bottomValue}>PHP {selPost?.price}.00</Typography>
+                                            <Typography className={classes.bottomTitle}>TICKET PRICE</Typography>
+                                        </Container>
+                                        <Container className={classes.date}>
+                                            <Typography className={classes.bottomValue}>{selPost?.date}</Typography>
+                                            <Typography className={classes.bottomTitle}>DATE</Typography>
+                                        </Container>
+                                    </Container>
+                                </Container>
+
+                            </Container>
+                        </Container>
+                    </Container>
+                </div>
+
+
+                {/* 2ND SECTION: MAIN PUB, CAPTION, AND REGISTRATION */}
+                <div>
+                    <Container className={classes.infoBox2}>
+
+                        <Container className={classes.left2}>       
+                            <Container className={classes.mainPub}>-insert main pub-{selPost?.file}</Container>
+                            <Container className={classes.caption}>
+                                -insert caption here-<br></br>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis sint magnam nam ex sed. Officiis sint in reprehenderit veniam exercitationem repellat expedita, vitae eum placeat laboriosam ipsum inventore itaque praesentium quisquam soluta unde quibusdam provident voluptatum dicta, aliquid illo hic, saepe modi. Sequi earum molestias illo consequuntur aliquid pariatur sed.
+                            </Container>
+                        </Container>
+
+                        <Container className={classes.right2}>
+                            <img className={classes.ticketbg} src={regBg} height="600"/>
+
+                            <Container className={classes.registration}>
+                               
+                                {(!selPost?.registeredUsers?.find((e)=> e==user.result._id) && !selPost?.acceptedUsers?.find((e)=> e==user.result._id))?(
+                                    <>
+                                    {(!user?.result?.dlsu || user?.result?.claimed) ? (
+                                        <form autoComplete='off' noValidate className={classes.form} onSubmit={handleSubmit}>
+                                        
+                                            
+                                            <Typography className={classes.regHeading}>REGISTER HERE</Typography>
+                                            
+                                            
+                                            <Typography className={classes.formTitle}>Full Name *</Typography>
+                                            <TextField/>
+
+                                            <Typography className={classes.formTitle}>DLSU Email *</Typography>
+                                            <TextField/>
+
+                                            <Typography className={classes.formTitle}>Complete ID Number *</Typography>
+                                            <TextField/>
+
+                                            <Typography className={classes.formTitle}>College *</Typography>
+                                            <TextField/>
+
+
+                                            {/* <Typography>Payment Details: 09270164346 GCASH JUSTIN TO</Typography> */}
+
+                                            <Typography className={classes.formPayment}>Proof of Payment *</Typography>
+                                            <div className={classes.fileInput}>
+                                                <FileBase type ="file"multiple={false} onDone={({base64})=> setFile(base64)}/>
+                                            </div>
+
+
+                                            <Button className={classes.buttonSubmit} variant="container" color="primary" size="large" type="submit" fullWidth>Register</Button>
+                                            
+                                        </form>
+                                    ): <Button className={classes.buttonSubmit} variant="container" color="primary" size="large" type="submit" onClick={handleSubmit} fullWidth>Register</Button> }
+                                    
+                                    
+                                    </>
+                                ):<Button className={classes.buttonSubmit} variant="container" color="primary" size="large" type="submit" onClick={handleSubmit} fullWidth>Unregister</Button> }
+                                 <Typography>STATUS: {txn.status}</Typography>
+                            </Container>
+                            
+                        </Container>
+                    </Container>
+                </div>
+            </main>
+
+
+
         </Grid>
+
+        // <Grid container alignItems="stretch" spacing={3}>
+            // {selPost?.title}
+            // {selPost?.description}
+            // {selPost?.price}
+            // {selPost?.date}
+            // {selPost?.tags}
+            // {selPost?.registeredUsers}
+            // <CssBaseline />
+            // <Typography>Status: {txn.status}</Typography>
+            // {(!selPost?.registeredUsers?.find((e)=> e==user.result._id) && !selPost?.acceptedUsers?.find((e)=> e==user.result._id))?(
+            //     <>
+            //     {(!user?.result?.dlsu || user?.result?.claimed) ? (
+            //         <form autoComplete='off' noValidate className={classes.form} onSubmit={handleSubmit}>
+            //             <Typography>Register for event:</Typography>
+            //             <Typography>Payment Details: 09270164346 GCASH JUSTIN TO</Typography>
+            //             <Typography>PROOF OF PAYMENT:</Typography>
+            //             <div className={classes.fileInput}>
+            //                 <FileBase type ="file"multiple={false} onDone={({base64})=> setFile(base64)}/>
+            //             </div>
+            //             <Button className={classes.buttonSubmit} variant="container" color="primary" size="large" type="submit" fullWidth>Register</Button>
+                        
+            //         </form>
+            //     ): <Button className={classes.buttonSubmit} variant="container" color="primary" size="large" type="submit" onClick={handleSubmit} fullWidth>Register</Button> }
+                
+                
+            //     </>
+            // ):<Button className={classes.buttonSubmit} variant="container" color="primary" size="large" type="submit" onClick={handleSubmit} fullWidth>Unregister</Button> }
+            
+        // </Grid>
     );
 };
 
--- client/src/components/PostDetails/styles.js
+++ client/src/components/PostDetails/styles.js
@@ -3,22 +3,167 @@ import { makeStyles } from '@material-ui/core/styles';
 export default makeStyles((theme) => ({
   root: {
     '& .MuiTextField-root': {
-      margin: theme.spacing(1),
+      // margin: theme.spacing(1),
     },
   },
   paper: {
-    padding: theme.spacing(2),
+    // padding: theme.spacing(2),
   },
-  form: {
+  title: {
+    color: '#fff',
+    fontSize: '10vh',
+    marginTop: '4vh',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    maxWidth: '100vw',
+    alignSelf: 'flexstart',
+    textTransform: 'uppercase',
+  },
+  infoBox: {
+    height: '600px',
+    borderRadius: '30px',
+    width: '90%',
+    position: 'relative',
+    backgroundColor: 'rgba(255, 255, 255, 0.232)',
     display: 'flex',
-    flexWrap: 'wrap',
+  },
+  innerInfo: {
+    backgroundColor: 'rgba(255, 255, 255, 0.167)',
+    height: '550px',
+    borderRadius: '30px',
+    width: '99%',
+    marginTop: '4vh',
+    display: 'flex',
+    flexDirection: 'row',
+    padding: '4vh',
+  },
+  innerBox: {
+    height: '100%',
+  },
+  left: {
+    // border: '1px solid white',
+    width: '35%',
+    textAlign: 'center',
+    padding: '18vh 4vh 18vh 4vh',
+    
+  },
+  right: {
+    // border: '1px solid white',
+    width: '65%',
+    padding: '5vh 5vh 5vh  0',
+    color: 'white',
+    display: 'flex',
     justifyContent: 'center',
   },
+  innerRight: {
+    height: 'auto',
+    // border: '1px solid pink',
+    color: 'white',
+    
+  },
+  descImg: {
+    border: '1px solid white',
+    height: '100%',
+    width: '100%',
+    borderRadius: '8px',
+  },
+  eventBy: {
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+  },
+  innerTitle: {
+    marginTop: '2vh',
+    fontSize: '5vh',
+  },
+  venue: {
+    textTransform: 'uppercase',
+    fontWeight: 'bold',
+    marginTop: '-1vh',
+    fontSize: '2.2vh'
+  },
+  description: {
+    marginTop: '2vh',
+  },
+  bottomInfo: {
+    display: 'flex',
+    flexDirection: 'row',
+    // border: '1px solid white',
+    padding: '0',
+    marginTop: '7vh',
+    position: 'absolute'
+  },
+  price: {
+    borderRight: '1px solid white',
+    width: '35vh',
+    
+  },
+  bottomValue: {
+    fontSize: '3vh',
+    fontWeight: 'bold',
+  },
+  bottomTitle: {
+    marginTop: '0.5vh',
+    fontSize: '2.5vh',
+  },
+  infoBox2: {
+    marginTop: '15vh',
+    // border: '1px solid white',
+    height: '90vh',
+    display: 'flex',
+    flexDirection: 'row',
+    color: 'white', 
+    // backgroundColor: 'rgba(0, 0, 0, 0.232)',
+    borderRadius: '30px'
+  },
+  left2: {
+    // border: '1px solid white',
+    padding: '5vh 5vh 5vh 5vh'
+  },
+  right2: {
+    // border: '1px solid white',
+    textAlign: 'center',
+    position: 'relative'
+  },
+  mainPub: {
+    border: '1px solid white',
+    height: '400px',
+    width: '300px',
+    borderRadius: '8px',
+    textAlign: 'center',
+  },
+  caption: {
+    marginTop: '3vh',
+    textAlign: 'center',
+  },
+  ticketBg: {
+    height: '10vh',
+  },
+  registration: {
+    position: 'absolute',
+    top: '6%',
+    right: '-2px',
+    // left: '50%',
+  },
+  regHeading: {
+    // marginTop: '1vh',
+    fontWeight: 'Bold',
+  }, 
+  formTitle: {
+    marginTop: '3vh',
+  },
+  formPayment: {
+    marginTop: '10vh'
+  },
   fileInput: {
-    width: '97%',
-    margin: '10px 0',
+    // border: '1px solid black',
+    width: '40%',
+    margin: '8px 0',
+    marginLeft: '26vh'
   },
   buttonSubmit: {
-    marginBottom: 10,
+    marginTop: '1vh',
+    marginBottom: '1vh',
+    backgroundColor: 'white',
+    width: '18%'
   },
 }));
