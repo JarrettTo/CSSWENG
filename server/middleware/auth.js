07e904061375f2f932a62f9ee42df4f118958aae
Thalyonn <thalyonns@gmail.com>
2022-12-10 19:58:35 +0800

    more fixes

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -8,7 +8,7 @@ import jwt from 'jsonwebtoken';
 * next: function
 * @author: Justin To and Daniel Capinpin
 */
-const auth= async(req, next)=>{
+const auth= async(req, res, next)=>{
     try {
         const token =req.headers.authorization.split(" ")[1];
         const isCustomAuth = token.length < 500;    //if token is less than 500 in length means its custom and not from google
